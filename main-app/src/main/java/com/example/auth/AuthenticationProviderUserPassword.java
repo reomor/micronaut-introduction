7fb2d7b8d8aa582452a297e75f6309e61e302667
Reomor <reomor@gmail.com>
2021-08-10 21:21:46 +0300

    logback -> log4j2

--- main-app/src/main/java/com/example/auth/AuthenticationProviderUserPassword.java
+++ main-app/src/main/java/com/example/auth/AuthenticationProviderUserPassword.java
@@ -9,34 +9,37 @@ import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.UserDetails;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Singleton;
 import java.util.ArrayList;
 
-@Slf4j
 @Singleton
 public class AuthenticationProviderUserPassword implements AuthenticationProvider {
-    @Override
-    public Publisher<AuthenticationResponse> authenticate(
-        HttpRequest<?> httpRequest,
-        AuthenticationRequest<?, ?> authenticationRequest
-    ) {
-        return Flowable.create(emitter -> {
-                Object identity = authenticationRequest.getIdentity();
-                Object secret = authenticationRequest.getSecret();
-                log.debug("User {} tries to authenticate by {}", identity, secret);
 
-                if (identity.equals("myuser") && secret.equals("secret")) {
-                    emitter.onNext(new UserDetails((String) identity, new ArrayList<>()));
-                    emitter.onComplete();
-                    return;
-                }
+  private static final Logger log = LogManager.getLogger(AuthenticationProviderUserPassword.class);
 
-                emitter.onError(new AuthenticationException(new AuthenticationFailed("Wrong name or password")));
-            },
-            BackpressureStrategy.ERROR
-        );
-    }
+  @Override
+  public Publisher<AuthenticationResponse> authenticate(
+    HttpRequest<?> httpRequest,
+    AuthenticationRequest<?, ?> authenticationRequest
+  ) {
+    return Flowable.create(emitter -> {
+        Object identity = authenticationRequest.getIdentity();
+        Object secret = authenticationRequest.getSecret();
+        log.debug("User {} tries to authenticate by {}", identity, secret);
+
+        if (identity.equals("myuser") && secret.equals("secret")) {
+          emitter.onNext(new UserDetails((String) identity, new ArrayList<>()));
+          emitter.onComplete();
+          return;
+        }
+
+        emitter.onError(new AuthenticationException(new AuthenticationFailed("Wrong name or password")));
+      },
+      BackpressureStrategy.ERROR
+    );
+  }
 }
--- main-app/src/main/java/com/example/controller/WatchListReactiveController.java
+++ main-app/src/main/java/com/example/controller/WatchListReactiveController.java
@@ -1,81 +1,79 @@
 package com.example.controller;
 
+import com.example.auth.AuthenticationProviderUserPassword;
 import com.example.domain.WatchList;
 import com.example.repository.AccountStore;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Delete;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.PathVariable;
-import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.reactivex.Flowable;
 import io.reactivex.Scheduler;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.inject.Named;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
 //@Secured(SecurityRule.IS_AUTHENTICATED)
-@Slf4j
 @Controller("/account/watchlist-reactive")
 public class WatchListReactiveController {
 
-    static final UUID ACCOUNT_ID = UUID.randomUUID();
+  private static final Logger log = LogManager.getLogger(AuthenticationProviderUserPassword.class);
 
-    private final Scheduler scheduler;
-    private final AccountStore accountStore;
+  static final UUID ACCOUNT_ID = UUID.randomUUID();
 
-    public WatchListReactiveController(
-        @Named(TaskExecutors.IO) ExecutorService executorService,
-        AccountStore accountStore
-    ) {
-        this.accountStore = accountStore;
-        this.scheduler = Schedulers.from(executorService);
-    }
+  private final Scheduler scheduler;
+  private final AccountStore accountStore;
 
-    @Get(
-        produces = MediaType.APPLICATION_JSON
-    )
-    @ExecuteOn(TaskExecutors.IO)
-    public WatchList get() {
-        log.info("Got request - {}", Thread.currentThread().getName());
-        return accountStore.getWatchList(ACCOUNT_ID);
-    }
+  public WatchListReactiveController(
+    @Named(TaskExecutors.IO) ExecutorService executorService,
+    AccountStore accountStore
+  ) {
+    this.accountStore = accountStore;
+    this.scheduler = Schedulers.from(executorService);
+  }
 
-    @Get(
-        value = "/single",
-        produces = MediaType.APPLICATION_JSON
-    )
-    public Single<WatchList> getSingle() {
-        log.info("Got request - {}", Thread.currentThread().getName());
-        return Single.fromCallable(() -> accountStore.getWatchList(ACCOUNT_ID))
+  @Get(
+    produces = MediaType.APPLICATION_JSON
+  )
+  @ExecuteOn(TaskExecutors.IO)
+  public WatchList get() {
+    log.info("Got request - {}", Thread.currentThread().getName());
+    return accountStore.getWatchList(ACCOUNT_ID);
+  }
+
+  @Get(
+    value = "/single",
+    produces = MediaType.APPLICATION_JSON
+  )
+  public Single<WatchList> getSingle() {
+    log.info("Got request - {}", Thread.currentThread().getName());
+    return Single.fromCallable(() -> accountStore.getWatchList(ACCOUNT_ID))
 //            .toFlowable()
-            .subscribeOn(scheduler);
-    }
+      .subscribeOn(scheduler);
+  }
 
-    @Put(
-        consumes = MediaType.APPLICATION_JSON,
-        produces = MediaType.APPLICATION_JSON
-    )
-    public Flowable<WatchList> update(@Body WatchList watchList) {
-        log.info("Got request - {}", Thread.currentThread().getName());
-        return Flowable.fromArray(accountStore.updateWatchList(ACCOUNT_ID, watchList));
-    }
+  @Put(
+    consumes = MediaType.APPLICATION_JSON,
+    produces = MediaType.APPLICATION_JSON
+  )
+  public Flowable<WatchList> update(@Body WatchList watchList) {
+    log.info("Got request - {}", Thread.currentThread().getName());
+    return Flowable.fromArray(accountStore.updateWatchList(ACCOUNT_ID, watchList));
+  }
 
-    @Delete(
-        value = "/{accountId}",
-        consumes = MediaType.APPLICATION_JSON,
-        produces = MediaType.APPLICATION_JSON
-    )
-    public Single<WatchList> delete(@PathVariable UUID accountId) {
-        log.info("Got request - {}", Thread.currentThread().getName());
-        return Single.fromCallable(() -> accountStore.deleteWatchList(accountId))
-            .subscribeOn(scheduler);
-    }
+  @Delete(
+    value = "/{accountId}",
+    consumes = MediaType.APPLICATION_JSON,
+    produces = MediaType.APPLICATION_JSON
+  )
+  public Single<WatchList> delete(@PathVariable UUID accountId) {
+    log.info("Got request - {}", Thread.currentThread().getName());
+    return Single.fromCallable(() -> accountStore.deleteWatchList(accountId))
+      .subscribeOn(scheduler);
+  }
 }
